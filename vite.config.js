d0748dc2a0b7b82828e7a18ef3545006bc069b36
Anthony Figueroa <tehleader1234@gmail.com>
2026-05-14 18:09:13 -0400

    Fix allowed hosts

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+    allowedHosts: [
+      'digitalhut.app',
+      'www.digitalhut.app'
+    ]
+  }
+})
