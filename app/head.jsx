f93b811ade24dde027067a7e8f3aa53c10a977fb
Anthony Figueroa <tehleader1234@gmail.com>
2026-05-27 19:59:35 -0400

    force searchatlas head injection

---
+++ app/head.jsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <script
+        id="sa-dynamic-optimization-loader"
+        data-uuid="fb51dd0f-e06f-457d-b7e5-952e02bdda6a"
+        src="https://dashboard.searchatlas.com/scripts/dynamic_optimization.js"
+        async
+      />
+    </>
+  )
+}
