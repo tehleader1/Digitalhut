dd7b903d4012ea1cc20744512cf1aef149f5c93f
Anthony Figueroa <tehleader1234@gmail.com>
2026-05-27 15:54:07 -0400

    fix jsx layout syntax

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,11 +2,7 @@ export const metadata = {
   title: "DigitalHut"
 }
 
-export default function RootLayout({
-  children,
-}:{
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
