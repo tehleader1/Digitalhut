e3fe58b463454ecfb6552bfdd06f43761d51fda3
tehleader1 <xxfigueroa1993@yahoo.com>
2026-06-08 10:05:26 -0700

    Wrap home renderer with live-view containment

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import PremiumLoadGate from "../components/PremiumLoadGate"
+import RendererContainmentGate from "../components/RendererContainmentGate"
 import FullscreenObservatoryV2 from "../components/FullscreenObservatoryV2"
 
 export default function HomePage(){
-  return <PremiumLoadGate><FullscreenObservatoryV2 /></PremiumLoadGate>
+  return <PremiumLoadGate><RendererContainmentGate><FullscreenObservatoryV2 /></RendererContainmentGate></PremiumLoadGate>
 }
