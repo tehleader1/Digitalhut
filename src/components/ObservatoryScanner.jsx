e607b170f99ca9ba0a641851f55e4e5d1b709e8d
tehleader1 <xxfigueroa1993@yahoo.com>
2026-06-08 18:15:52 -0700

    Wire guided tour runtime into scanner

--- src/components/ObservatoryScanner.jsx
+++ src/components/ObservatoryScanner.jsx
@@ -1,7 +1,8 @@
 import PremiumLoadGate from "./PremiumLoadGate"
 import RendererContainmentGate from "./RendererContainmentGate"
+import GuidedTourRuntime from "./GuidedTourRuntime"
 import FullscreenObservatoryV2 from "./FullscreenObservatoryV2"
 
 export default function ObservatoryScanner(){
-  return <PremiumLoadGate><RendererContainmentGate><FullscreenObservatoryV2 /></RendererContainmentGate></PremiumLoadGate>
+  return <PremiumLoadGate><RendererContainmentGate><GuidedTourRuntime><FullscreenObservatoryV2 /></GuidedTourRuntime></RendererContainmentGate></PremiumLoadGate>
 }
